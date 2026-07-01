283fb1c8473eff4458e92e4e8204114cadc936ad
KSJ Hub <sycomorgan@gmail.com>
2026-07-01 06:47:10 +0100

    Remove unused preload bridge variant

--- electron/preload.cjs
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
-contextBridge.exposeInMainWorld('nexusDesktop', {
-  copyText: (text) => ipcRenderer.invoke('nexus:copy-text', text),
-  openExternal: (url) => ipcRenderer.invoke('nexus:open-external', url),
-  openWorkspace: (workspacePath) => ipcRenderer.invoke('nexus:open-workspace', workspacePath),
-})
