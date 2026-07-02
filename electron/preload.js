eb3d3634584872f8a50b58ed25ca666e7184f42e
KSJ Hub <sycomorgan@gmail.com>
2026-07-02 04:20:12 +0100

    v1.4.0 - Add window controls bridge

--- electron/preload.js
+++ electron/preload.js
@@ -4,4 +4,8 @@ contextBridge.exposeInMainWorld('nexusDesktop', {
   copyText: (text) => ipcRenderer.invoke('nexus:copy-text', text),
   openExternal: (url) => ipcRenderer.invoke('nexus:open-external', url),
   openWorkspace: (workspacePath) => ipcRenderer.invoke('nexus:open-workspace', workspacePath),
+  windowClose: () => ipcRenderer.invoke('nexus:window-close'),
+  windowMinimise: () => ipcRenderer.invoke('nexus:window-minimise'),
+  windowToggleAlwaysOnTop: () => ipcRenderer.invoke('nexus:window-toggle-always-on-top'),
+  windowToggleSize: () => ipcRenderer.invoke('nexus:window-toggle-size'),
 })
