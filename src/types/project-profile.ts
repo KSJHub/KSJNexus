25f863fbed4346291b9454f7ac649ded07b6454c
KSJ Hub <sycomorgan@gmail.com>
2026-07-02 02:41:52 +0100

    v1.2.0 - Add project profile type

---
+++ src/types/project-profile.ts
@@ -0,0 +1,15 @@
+export type ProjectProfile = {
+  id: string
+  name: string
+  description: string
+  icon: string
+  color: string
+  repository: string
+  repositoryUrl: string
+  workspace: string
+  workspacePath: string
+  chatGptUrl: string
+  websiteUrl: string
+  discordUrl: string
+  notes: string
+}
